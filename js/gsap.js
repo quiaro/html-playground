3a13836c2f7bae8c979b286fdc9c26b56bfbf7e9
quiaro <quiaro@gmail.com>
2016-10-31 11:49:31 -0600

    add emphasis animation; use GSAP's TimelineMax

--- js/gsap.js
+++ js/gsap.js
@@ -1,7 +1,7 @@
 /* eslint-env browser */
 /* eslint max-len: ["error", 120, { "ignoreComments": true }] */
 
-/* global jQuery, TweenMax */
+/* global jQuery, TimelineMax */
 
 /*
  * Animation samples using Greensock's TweenMax library
@@ -10,19 +10,52 @@
   'use strict';
 
   /* Hello world animation */
-  var elem = document.querySelector('.pulse-gsap');
-  TweenMax.fromTo(elem, 2, {
+  var pulseEl = document.querySelector('.pulse-gsap');
+  var pulseTl = new TimelineMax({
+    delay: 0.01,
+    repeat: -1,
+    yoyo: true
+  });
+  pulseTl.to(pulseEl, 2, {
     opacity: 0.5,
     scale: 0.5,
-    repeat: -1,
-    yoyo: true,
     ease: 'Linear'
-  }, {
+  })
+  .to(pulseEl, 2, {
     opacity: 1,
     scale: 1,
-    repeat: -1,
-    yoyo: true,
     ease: 'Linear'
   });
   /* EO: Hello world animation */
+
+  /* Emphasis animation */
+  var emphasisEl = document.querySelector('.emphasis-gsap');
+  var emphasisTl = new TimelineMax({
+    delay: 0.1,
+    repeat: -1,
+    yoyo: true
+  });
+
+  emphasisTl.to(emphasisEl, 0.350, {
+    opacity: 0.5,
+    scale: 0.5,
+    ease: 'Power2.easeInOut'
+  })
+  .to(emphasisEl, 0.300, {
+    opacity: 0.667,
+    scale: 0.667,
+    ease: 'Power2.easeInOut'
+  })
+  .to(emphasisEl, 0.300, {
+    opacity: 0.6,
+    scale: 0.6,
+    ease: 'Power2.easeInOut'
+  })
+  .to(emphasisEl, 0.350, {
+    opacity: 1,
+    scale: 1,
+    ease: 'Power2.easeInOut'
+  });
+
+  /* EO: Emphasis animation */
 })(jQuery);
--- js/wajs.js
+++ js/wajs.js
@@ -21,4 +21,20 @@
     iterations: Infinity
   });
   /* EO: Hello world animation */
+
+  /* Emphasis animation */
+  document.querySelector('.emphasis-wajs').animate([
+    {transform: 'scale(1)', opacity: 1, offset: 0},
+    {transform: 'scale(.5)', opacity: .5, offset: .3},
+    {transform: 'scale(.667)', opacity: .667, offset: .7875},
+    {transform: 'scale(.6)', opacity: .6, offset: 1}
+  ], {
+    duration: 700, // milliseconds
+    easing: 'ease-in-out', // 'linear', a bezier curve, etc.
+    delay: 10, // milliseconds
+    iterations: Infinity,
+    direction: 'alternate', // 'normal', 'reverse', etc.
+    fill: 'forwards' // 'backwards', 'both', 'none', 'auto'
+  });
+  /* EO: Emphasis animation */
 })(jQuery);
